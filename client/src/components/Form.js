366d09c194d36fc85e58382ce77b08c0dd826f77
Orion Weldon <orion@Orions-MacBook-Pro.local>
2018-01-24 13:06:47 -0800

    stubbed out form, formfive and home

---
+++ client/src/components/Form.js
@@ -0,0 +1,16 @@
+export React from 'react';
+
+
+class Form extennd Component {
+render() {
+  return(
+    <div>
+      <h1>
+        This will be the first form. Known as FormFive
+      </h1>
+    </div>
+    );
+  }
+}
+
+export default Form;
---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+export React from 'react';
+
+
+class Home extennd Component {
+render() {
+  return(
+    <div>
+      <h1>
+        This will be the home page
+      </h1>
+    </div>
+    );
+  }
+}
+
+export default Home;
