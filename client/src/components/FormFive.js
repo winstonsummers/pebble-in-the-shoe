6dd5ba87b630bebe47304057c4c4ad3061a16d8c
JustinPMitchell <j.mitchell.real@gmail.com>
2018-01-24 14:18:11 -0800

    Components flushed out

--- client/src/components/FormFive.js
+++ client/src/components/FormFive.js
@@ -1,13 +1,20 @@
-export React from 'react';
+import React, {Component} from 'react';
 
 
-class FormFive extennd Component {
+class FormFive extends Component {
 render() {
   return(
     <div>
-      <h1>
-        This will be FormFive
-      </h1>
+      <h1>Indecisive Doves</h1>
+      <h2>User Made Title</h2>
+      <form>
+        <h2>5-Ideas</h2>
+        <input type='checkbox'></input>User Made Item1
+        <input type='checkbox'></input>User Made Item2
+        <input type='checkbox'></input>User Made Item3
+        <input type='checkbox'></input>User Made Item4
+        <input type='checkbox'></input>User Made Item5
+      </form>
     </div>
     );
   }
