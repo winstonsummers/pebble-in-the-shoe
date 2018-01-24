8294b8cde27af1ab9ac97173ab4a830744585a0b
JustinPMitchell <j.mitchell.real@gmail.com>
2018-01-24 13:03:34 -0800

    stubbed out Start FormTwo, and Decision

---
+++ client/src/components/Decision.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Decision extends Component {
+  render(){
+    return (<div>
+        <h2>Start coming soon!</h2>
+      </div>
+    );
+  }
+}
+
+export default Decision;
